334729a204152ad67c18fefab2d42594d4bdb29a
cprejler <prejler93@gmail.com>
2020-04-15 09:19:07 +0200

    Merge

--- src/main/java/rest/ApplicationConfig.java
+++ src/main/java/rest/ApplicationConfig.java
@@ -25,10 +25,10 @@ public class ApplicationConfig extends Application {
         resources.add(errorhandling.AuthenticationExceptionMapper.class);
         resources.add(errorhandling.GenericExceptionMapper.class);
         resources.add(org.glassfish.jersey.server.wadl.internal.WadlResource.class);
+        resources.add(rest.CatFactResource.class);
         resources.add(rest.DemoResource.class);
         resources.add(rest.PokeResource.class);
         resources.add(rest.RenameMeResource.class);
-        resources.add(rest.CatFactResource.class);
         resources.add(security.JWTAuthenticationFilter.class);
         resources.add(security.LoginEndpoint.class);
         resources.add(security.RolesAllowedFilter.class);
