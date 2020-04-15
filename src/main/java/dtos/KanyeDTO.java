acc707a4ce70900cc0114e82032f256d630c2a9f
Plebbz <jens.ohlendorff@gmail.com>
2020-04-15 09:14:39 +0200

    kanye rest implementeret

---
+++ src/main/java/dtos/KanyeDTO.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dtos;
+
+/**
+ *
+ * @author jenso
+ */
+public class KanyeDTO {
+    
+    private String text;
+    private String createdAt;
+
+    public KanyeDTO(String text, String createdAt) {
+        this.text = text;
+        this.createdAt = createdAt;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+    
+    
+    
+    
+}
