13aa731fa260877dedcc83b8ef82993811964fd4
ATThorsen <thorsen.aske@gmail.com>
2020-04-15 09:32:23 +0200

    uselessfact

---
+++ src/main/java/dtos/UselessFactDTO.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dtos;
+
+/**
+ *
+ * @author Aske
+ */
+public class UselessFactDTO {
+    
+    String id;
+    String text;
+
+    public UselessFactDTO(String id, String text) {
+        this.id = id;
+        this.text = text;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    
+
+}
