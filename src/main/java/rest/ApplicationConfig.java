9e7c15b9ea3daaf11ae13e26526c973349407929
Malthorn1 <mikkelskott@gmail.com>
2020-04-17 09:38:07 +0200

    kom til at slette catfactresource

--- src/main/java/rest/ApplicationConfig.java
+++ src/main/java/rest/ApplicationConfig.java
@@ -25,6 +25,7 @@ public class ApplicationConfig extends Application {
         resources.add(errorhandling.AuthenticationExceptionMapper.class);
         resources.add(errorhandling.GenericExceptionMapper.class);
         resources.add(org.glassfish.jersey.server.wadl.internal.WadlResource.class);
+        resources.add(rest.CatFactResource.class);
         resources.add(rest.DemoResource.class);
         resources.add(rest.KanyeResource.class);
         resources.add(rest.PokeResource.class);
